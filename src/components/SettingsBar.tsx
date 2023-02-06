7aa8da59439fdba425c3a1dcf22a4e1148655c9d
pavel <kovrizhko@gmail.com>
2023-02-07 01:54:19 +0300

    feat: add line width in settings bar

--- src/components/SettingsBar.tsx
+++ src/components/SettingsBar.tsx
@@ -1,8 +1,25 @@
-import React, { FC } from 'react';
+import React, { ChangeEvent, FC } from 'react';
 import '../styles/settingsBar.scss';
+import ToolStore from '../store/toolStore';
 
 const SettingsBar: FC = () => {
-  return <div className="settingsBar"></div>;
+  const onInputChange = (evt: ChangeEvent<HTMLInputElement>): void => {
+    ToolStore.setWidth(+evt.target.value);
+  };
+
+  return (
+    <div className="settingsBar">
+      <label htmlFor="inputWeight">Толщина линии</label>
+      <input
+        id="inputWeight"
+        type="number"
+        defaultValue={1}
+        min={1}
+        max={50}
+        onChange={onInputChange}
+      />
+    </div>
+  );
 };
 
 export default SettingsBar;
